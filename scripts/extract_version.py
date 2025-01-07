b5be44583507932945d6d9a6ec29b565f64e37e9
stocktonjenkins <stockton.jenkins@deliciousai.com>
2025-01-07 11:11:05 -0700

    chore: bump

--- scripts/extract_version.py
+++ scripts/extract_version.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
     fp = "./scripts/__init__.py"
     with open(fp, "w") as version_file:
         version_file.write(f"__version__ = \"{version}\"\n")
-        # testing..
